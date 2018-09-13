7d2634d115f0f24c7c9ce8cbeb70afbf00a0a35c
wmzy <1256573276@qq.com>
2018-09-13 17:17:20 +0800

    load packages on start

--- repl.js
+++ repl.js
@@ -1,14 +1,53 @@
 const os = require('os');
+const fs = require('fs');
 const path = require('path');
 const {execSync} = require('child_process');
+const util = require('util');
+const rmdir = require('rimraf');
 const cliRepl = require('./internal/repl');
 
+const debug = util.debuglog('repl-enhance');
+
 const contextPath = path.join(os.homedir(), '.repl-enhance');
+const packageJSONPath = path.join(contextPath, 'package.json');
 const packagePath = path.join(contextPath, 'node_modules');
-const reg = /^(?:const|var|let)\s+(\w)\s*=\s*require\(['"](.*)['"]\)[;\s]*$/;
+const reg = /^(?:const|var|let)\s+(\w)\s*=\s*require\(['"]([^./].*)['"]\)[;\s]*$/;
+
+function noop() {}
+
+module.paths.push(packagePath);
+
+let packageJSON = {
+  dependencies: {}
+};
+
+try {
+  // eslint-disable-next-line global-require, import/no-dynamic-require
+  packageJSON = require(packageJSONPath);
+} catch (e) {
+  fs.mkdir(contextPath, () => {
+    fs.writeFile(packageJSONPath, JSON.stringify(packageJSON), noop);
+  });
+}
 
 cliRepl.createInternalRepl(process.env, (err, repl) => {
   if (err) throw err;
+
+  repl.defineCommand('clean', {
+    help: 'remove all cached packages',
+    action: clean
+  });
+  repl.defineCommand('update', {
+    help: 'update [package]\n\t\tupdate cached packages',
+    action: update
+  });
+
+  Object.keys(packageJSON.dependencies).forEach(dep => {
+    if (!repl.context[dep]) {
+      // eslint-disable-next-line no-param-reassign, global-require, import/no-dynamic-require
+      repl.context[dep] = require(dep);
+    }
+  });
 
   repl.context.module.paths.push(packagePath);
 
@@ -20,7 +59,12 @@ cliRepl.createInternalRepl(process.env, (err, repl) => {
       .join(' ')
       .match(reg);
     if (m) {
-      install(m[2]);
+      const pkg = m[2];
+      try {
+        require.resolve(pkg);
+      } catch (e) {
+        install(pkg);
+      }
     }
     defaultEval(cmd, context, filename, callback);
   };
@@ -35,10 +79,23 @@ cliRepl.createInternalRepl(process.env, (err, repl) => {
 });
 
 function install(pkg) {
-  console.log(contextPath);
   try {
     execSync(`npm i ${pkg}`, {cwd: contextPath, stdio: 'inherit'});
   } catch (e) {
-    console.error(e);
+    debug(e.stack);
   }
+}
+
+function clean() {
+  rmdir(contextPath, noop);
+  this.displayPrompt();
+}
+
+function update(pkg) {
+  try {
+    execSync(`npm update ${pkg}`, {cwd: contextPath, stdio: 'inherit'});
+  } catch (e) {
+    debug(e.stack);
+  }
+  this.displayPrompt();
 }
